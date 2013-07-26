e613f602ec078d4a2d5ae7dd9b3499a94b1d88c7
Soren Kuula <soren.kuula@gmail.com>
2013-07-26 11:53:31 +0200

    blah

--- otherpart.c
+++ otherpart.c
@@ -3,12 +3,13 @@
  * foo --tty=/dev/ttyO4 --baud=19200 --remote=dongfang.ch:14550 --remote=udp:localhost:14550 --remote=tcp:192.168.1.1:10000 --mavlink --logging
  */
 
-#include <ctype.h>
+//#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdint.h>
 #include <getopt.h>
+#include <string.h>
 
 typedef uint8_t bool;
 #define true 1
@@ -29,29 +30,42 @@ int main(int argc, char** argv) {
   bool doesLogging = false;
   bool didGetPort = false;
   bool didGetRemote = false;
+  char port[100];
+  int baudrate = 57600;
+
   int c;
 
-  struct option longopts[2] = {
+  struct option longopts[] = {
     (struct option){.name="port", .has_arg=1, .flag=NULL, .val=200},
+    (struct option){.name="remote", .has_arg=1, .flag=NULL, .val=201},
+    (struct option){.name="baudrate", .has_arg=1, .flag=NULL, .val=202},
+    (struct option){.name="mavlink", .has_arg=0, .flag=NULL, .val=203},
+    (struct option){.name="debug", .has_arg=1, .flag=NULL, .val=204},
     (struct option){.name=NULL, .has_arg=0, .flag=NULL, .val=0}
   };
 
-  while ((c = getopt_long(argc, argv, "p:r:md:", longopts, NULL)) != -1) {
+  while ((c = getopt_long(argc, argv, "p:r:mdb:", longopts, NULL)) != -1) {
     switch (c)
       {
       case 'p':
       case 200:
-	parsePort(optarg);
+	strcpy(port, optarg);
 	didGetPort = true;
 	break;
+      case 'b':
+      case 202:
+	baudrate = atoi(optarg);
       case 'r':
+      case 201:
 	parseRemote(optarg);
 	didGetRemote = true;
 	break;
       case 'm':
+      case 203:
 	isMAVLink = true;
 	break;
       case 'd':
+      case 204:
 	doesLogging = true;
 	break;
 	/*
